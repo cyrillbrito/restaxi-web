ad0869634754661e9030118a7930b3c655ae9ba0
Cyrill Brito <cyrill.brito@gmail.com>
2019-02-16 15:42:06 +0000

    Added language selector

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,25 @@
 import './styles.css';
 
-const func = () => {
-  console.log(__("HELLO_WORLD"));
-  console.log(__("Missing Text"));
-};
+let language;
 
-func();
+function init() {
+  detectLanguage();
 
+  const languageElement = document.getElementById(language + '-a');
+  languageElement.classList.add('selected-lang-a');
+  languageElement.href = 'javascript:;';
+}
+
+function detectLanguage() {
+  const url = window.location.href;
+  if (url.includes('/en')) {
+    language = 'en';
+  } else if (url.includes('/fr')) {
+    language = 'fr';
+  } else {
+    language = 'pt';
+  }
+}
+
+// main
+init();
